39cf3d377c198ea6b264c1cde0cb77286333ab22
gellen <hellogellen@gmail.com>
2020-08-09 20:47:24 +0700

    initiate project dengan CI, menambahkan halaman beranda #1
    - initiate project dengan framework PHP CI
    - tampilan beranda #1

---
+++ application/views/beranda/index.php
@@ -0,0 +1,32 @@
+<div class="section light-blue darken-2 hero-beranda">
+    <div class="container">
+        <div class="row">
+            <div class="col m5 s12">
+                <br><br><br><br>
+                <h5 class="header left white-text">STIKI Computer Networking Club</h5>
+                <p class="left white-text">
+                    Salah Satu Unit Kegiatan Mahasiswa di Sekolah Tinggi Informatika & Komputer Indonesia Malang yang bergerak di bidang Jaringan Komputer.
+                    <br><br><br>
+                </p>
+                <div class="left">
+                    <a href="#" class="waves-effect waves-light btn light-blue darken-3">Jelajahi</a>
+                </div>
+            </div>
+            <div class="col m5 offset-m2 s12 hide-on-small-only">
+                <br><br><br><br><br>
+                <div class="valign-wrapper">
+                    <img src="<?= base_url('assets/img/logo.png'); ?>" width="400" />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="section">
+    <div class="container">
+        <div class="row">
+            <div class="col s12">
+                <h5 class="header center-align grey-text text-darken-4">Kontribusi</h5>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ application/views/template/header.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0" />
+    <title>SCeN</title>
+
+    <!-- CSS  -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" media="screen,projection">
+    <link href="<?= base_url('assets/css/style.css'); ?>" type="text/css" rel="stylesheet" media="screen,projection" />
+</head>
+
+<body>
+    <nav class="light-blue darken-2" role="navigation">
+        <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">SCeN</a>
+            <ul class="right hide-on-med-and-down">
+                <li><a href="<?= base_url(); ?>">Beranda</a></li>
+                <li><a href="#">Tentang</a></li>
+                <li><a href="#">Kontak</a></li>
+                <li><a class="waves-effect waves-light btn light-blue darken-3" href="#">Blog</a></li>
+            </ul>
+
+            <ul id="nav-mobile" class="sidenav">
+                <li><a href="<?= base_url(); ?>">Beranda</a></li>
+                <li><a href="#">Pelayanan</a></li>
+                <li><a href="#">Kontak</a></li>
+            </ul>
+            <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+        </div>
+    </nav>
