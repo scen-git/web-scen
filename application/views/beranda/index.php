8f0b0cd221b0dc331431498fd4091a1ca6344602
gellen <hellogellen@gmail.com>
2020-08-10 23:05:42 +0700

    beranda #2
    - menambahkan beberapa konten di beranda
    - update footer

--- application/views/beranda/index.php
+++ application/views/beranda/index.php
@@ -1,3 +1,4 @@
+<!-- Hero Beranda -->
 <div class="section light-blue darken-2 hero-beranda">
     <div class="container">
         <div class="row">
@@ -21,6 +22,9 @@
         </div>
     </div>
 </div>
+<!-- End Hero Beranda -->
+
+<!-- Kontribusi -->
 <div class="section">
     <div class="container">
         <div class="row">
@@ -28,5 +32,80 @@
                 <h5 class="header center-align grey-text text-darken-4">Kontribusi</h5>
             </div>
         </div>
+        <div class="row">
+            <div class="col s12 m4 offset-m4">
+                <div class="card-panel deep-purple accent-1">
+                    <h6 class="white-text">
+                        <strong>KonPETI</strong>
+                    </h6>
+                    <p class="white-text">
+                        Konferensi Pekerja Teknologi Informasi.
+                    </p>
+                    <small class="white-text">18 April 2020</small>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
+<!-- End Kontribusi -->
+
+<!-- Kegiatan Terbaru -->
+<div class="section">
+    <div class="container">
+        <div class="row">
+            <div class="col s12">
+                <h5 class="header center-align grey-text text-darken-4">Kegiatan Terbaru</h5>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col m4 s12">
+                <div class="card light-blue darken-2">
+                    <div class="card-image">
+                        <img src="<?= base_url('assets/img/uploads/serit.jpeg'); ?>">
+                    </div>
+                    <div class="card-content white-text">
+                        <span class="card-title">SERIT <small>(SCeN Ngabuburit)</small></span>
+                        <p>
+                            Program SCeN yang bertujuan untuk berbagi ilmu pada waktu ngabuburit.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col m4 s12">
+                <div class="card light-blue darken-2">
+                    <div class="card-image">
+                        <img src="<?= base_url('assets/img/uploads/konpeti.png'); ?>">
+                    </div>
+                    <div class="card-content white-text">
+                        <span class="card-title">KonPETI</span>
+                        <p>
+                            Konferensi Pekerja Teknologi Informasi.
+                        </p>
+                    </div>
+                    <div class="card-action">
+                        <a href="https://gerakdarirumah.id/konpeti/" target="_blank" class="white-text"><u>Lebih Banyak</u></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col m4 s12">
+                <div class="card light-blue darken-2">
+                    <div class="card-image">
+                        <img src="<?= base_url('assets/img/uploads/diklatruang.jpg'); ?>">
+                    </div>
+                    <div class="card-content white-text">
+                        <span class="card-title">Diklat Anggota Baru</span>
+                        <p>
+                            Acara tahunan yang bertujuan untuk mengenalkan anggota baru SCeN dengan UKM SCeN itu sendiri.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s12 m2 offset-m5">
+                <a class="waves-effect waves-light btn-large light-blue darken-3" href="#">Lainnya</a>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Kegiatan Terbaru -->
--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,3 +1,32 @@
+<footer class="page-footer light-blue darken-2">
+    <div class="container">
+        <div class="row">
+            <div class="col m4 s12">
+                <h5 class="white-text">STIKI Computer Networking Club</h5>
+                <p class="grey-text text-lighten-4">&quot;Bravo SCeN!&quot;</p>
+            </div>
+            <div class="col m3 offset-m2 s12">
+                <h5 class="white-text">Lainnya</h5>
+                <ul>
+                    <li><a class="grey-text text-lighten-3" href="#!">Kepengurusan</a></li>
+                    <li><a class="grey-text text-lighten-3" href="#!">Peraturan Sekret</a></li>
+                </ul>
+            </div>
+            <div class="col m3 s12">
+                <h5 class="white-text">Media Sosial</h5>
+                <ul>
+                    <li><a class="grey-text text-lighten-3" href="#!">Instagram</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container">
+            © 2000-2020 SCeN
+        </div>
+    </div>
+</footer>
+
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 <script src="<?= base_url('assets/js/init.js'); ?>"></script>
